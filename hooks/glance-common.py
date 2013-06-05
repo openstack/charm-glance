9b663088504439245d364ab6ee5d5ad8832a8d75
Andres Rodriguez <andreserl@ubuntu.com>
2013-06-05 13:32:25 -0400

    configure_https

--- hooks/glance-common.py
+++ hooks/glance-common.py
@@ -8,14 +8,22 @@ from lib.openstack_common import (
 
 from lib.utils import (
     relation_ids,
+    install,
+    stop,
+    )
+
+from lib.cluster_utils import (
+    is_clustered,
+    determine_haproxy_port,
+    determine_api_port,
     )
 
 from glance-relations import (
-     db_changed,
-     keystone_changed,
-     ceph_changed,
-     object-store_changed,
-     )
+    db_changed,
+    keystone_changed,
+    ceph_changed,
+    object-store_changed,
+    )
 
 CHARM = "glance"
 
@@ -30,6 +38,41 @@ CONF_DIR = "/etc/glance"
 
 # Flag used to track config changes.
 CONFIG_CHANGED =  False
+
+def execute(cmd, die=False, echo=False):
+    """ Executes a command
+
+    if die=True, script will exit(1) if command does not return 0
+    if echo=True, output of command will be printed to stdout
+
+    returns a tuple: (stdout, stderr, return code)
+    """
+    p = subprocess.Popen(cmd.split(" "),
+                         stdout=subprocess.PIPE, 
+                         stdin=subprocess.PIPE,
+                         stderr=subprocess.PIPE)
+    stdout = ""
+    stderr = ""
+
+    def print_line(l):
+        if echo:
+            print l.strip('\n')
+            sys.stdout.flush()
+
+    for l in iter(p.stdout.readline, ''):
+        print_line(l)
+        stdout += l
+    for l in iter(p.stderr.readline, ''):
+        print_line(l)
+        stderr += l
+
+    p.communicate()
+    rc = p.returncode
+
+    if die and rc != 0:
+        error_out("ERROR: command %s return non-zero.\n" % cmd)
+    return (stdout, stderr, rc)
+
 
 function set_or_update {
   local key="$1"
@@ -89,56 +132,61 @@ def do_openstack_upgrade(install_src, packages):
     r_id = relation_ids('shared-db')
     if r_id:
         juju_log('INFO', '%s: Configuring database after upgrade to %s.' % (CHARM, install_src))
-        db_changed(ids)
+        db_changed(rid=r_id)
 
     r_id = relation_ids('identify-server')
     if r_id:
         juju_log('INFO', '%s: Configuring identity service after upgrade to %s' % (CHARM, install_src))
-        keystone_changed(rid)
+        keystone_changed(rid=r_id)
 
     ceph_ids = relation_ids('ceph')
     if ceph_ids:
         install('ceph-common', 'python-ceph')
         for r_id in ceph_ids:
             for unit in relation_list(rid):
-                ceph_changed(r_id, unit)
+                ceph_changed(rid=r_id, unit=unit)
 
     r_id = relation_ids('object-store')
     if r_id:
         object-store_joined()
 
 
-configure_https() {
-  # request openstack-common setup reverse proxy mapping for API and registry
-  # servers
-  service_ctl glance-api stop
-  if [[ -n "$(peer_units)" ]] || is_clustered ; then
-    # haproxy may already be configured. need to push it back in the request
-    # pipeline in preparation for a change from:
-    #  from:  haproxy (9292) -> glance_api (9282)
-    #  to:    ssl (9292) -> haproxy (9291) -> glance_api (9272)
-    local next_server=$(determine_haproxy_port 9292)
-    local api_port=$(determine_api_port 9292)
-    configure_haproxy "glance_api:$next_server:$api_port"
-  else
-    # if not clustered, the glance-api is next in the pipeline.
-    local api_port=$(determine_api_port 9292)
-    local next_server=$api_port
-  fi
+def configure_https():
+    # request openstack-common setup reverse proxy mapping for API and registry
+    # servers
+    stop('glance-api')
 
-  # setup https to point to either haproxy or directly to api server, depending.
-  setup_https 9292:$next_server
+    if len(peer_units()) > 0 or is_clustered():
+        # haproxy may already be configured. need to push it back in the request
+        # pipeline in preparation for a change from:
+        #  from:  haproxy (9292) -> glance_api (9282)
+        #  to:    ssl (9292) -> haproxy (9291) -> glance_api (9272)
+        next_server = determine_haproxy_port('9292')
+        api_port = determine_api_port('9292')
+        service_ports = {
+            "glance_api": [
+                next_server,
+                api_port
+                ]
+            }
+        configure_haproxy(service_ports)
+    else:
+        # if not clustered, the glance-api is next in the pipeline.
+        api_port = determine_api_port('9292')
+        next_server = api_port
 
-  # configure servers to listen on new ports accordingly.
-  set_or_update bind_port "$api_port" "api"
-  service_ctl all start
+    # TODO:
+    # setup_https 9292:$next_server
+    # setup https to point to either haproxy or directly to api server, depending.
+    setup_https 9292:$next_server
 
-  local r_id=""
-  # (re)configure ks endpoint accordingly in ks and nova.
-  for r_id in $(relation-ids identity-service) ; do
-    keystone_joined "$r_id"
-  done
-  for r_id in $(relation-ids image-service) ; do
-    image-service_joined "$r_id"
-  done
-}
+    # TODO:
+    # configure servers to listen on new ports accordingly.
+    # set_or_update bind_port "$api_port" "api"
+    set_or_update()
+    start(SERVICES)
+
+    for r_id in relation_ids('identity-service'):
+        keystone_joined(relation_id=r_id)
+    for r_id in relation_ids('image-service'):
+        image-service_joined(relation_id=r_id)
