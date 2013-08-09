b15961e8556e567141559a3af8e3cd8162f5afff
Andres Rodriguez <andreserl@ubuntu.com>
2013-08-09 15:29:00 -0400

    Add more tests

--- tests/test_glance_relations.py
+++ tests/test_glance_relations.py
@@ -28,6 +28,7 @@ TO_PATCH = [
     'apt_install',
     'apt_update',
     'restart_on_change',
+    'service_stop',
     #charmhelpers.contrib.openstack.utils
     'configure_installation_source',
     'get_os_codename_package',
@@ -35,15 +36,15 @@ TO_PATCH = [
     'eligible_leader',
     'is_clustered',
     # glance_utils
-    'PACKAGES',
     'restart_map',
     'register_configs',
     'do_openstack_upgrade',
     'migrate_database',
     # glance_relations
     'configure_https',
-    #'GLANCE_REGISTRY_CONF',
-    #'GLANCE_API_CONF',
+    # other
+    'getstatusoutput',
+    'check_call',
 ]
 
 
@@ -52,14 +53,16 @@ class GlanceRelationTests(CharmTestCase):
         super(GlanceRelationTests, self).setUp(relations, TO_PATCH)
         self.config.side_effect = self.test_config.get
 
-#    def test_install_hook(self):
-#        repo = 'cloud:precise-grizzly'
-#        self.test_config.set('openstack-origin', repo)
-#        self.PACKAGES.return_value = ['foo', 'bar']
-#        relations.install_hook()
-#        self.configure_installation_source.assert_called_with(repo)
-#        self.assertTrue(self.apt_update.called)
-#        self.apt_install.assert_called_with(['foo', 'bar'], fatal=True)
+    def test_install_hook(self):
+        repo = 'cloud:precise-grizzly'
+        self.test_config.set('openstack-origin', repo)
+        self.service_stop.return_value = True
+        relations.install_hook()
+        self.configure_installation_source.assert_called_with(repo)
+        self.assertTrue(self.apt_update.called)
+        self.apt_install.assert_called_with(['apache2', 'glance', 'python-mysqldb',
+                                             'python-swift', 'python-keystone',
+                                             'uuid', 'haproxy'])
 
     def test_db_joined(self):
         self.unit_get.return_value = 'glance.foohost.com'
@@ -84,7 +87,7 @@ class GlanceRelationTests(CharmTestCase):
         relations.db_changed()
 
     @patch.object(relations, 'CONFIGS')
-    def test_db_changed_on_essex(self, configs):
+    def test_db_changed_no_essex(self, configs):
         self._shared_db_test(configs)
         self.assertEquals([call('/etc/glance/glance-registry.conf'),
                            call('/etc/glance/glance-api.conf')],
@@ -94,12 +97,32 @@ class GlanceRelationTests(CharmTestCase):
         )
         self.migrate_database.assert_called_with()
 
-    #@patch.object(relations, 'CONFIGS')
-    #def test_db_changed_no_essex(self, configs):
-    #    self.get_os_codename_package.return_value = "other"
-    #    self._shared_db_test(configs)
-    #    self.assertEquals([call('/etc/glance/glance-registry.conf')],
-    #                       configs.write.call_args_list)
+    @patch.object(relations, 'CONFIGS')
+    def test_db_changed_with_essex_not_setting_version_control(self, configs):
+        self.get_os_codename_package.return_value = "essex"
+        self.getstatusoutput.return_value = (0, "version")
+        self._shared_db_test(configs)
+        self.assertEquals([call('/etc/glance/glance-registry.conf')],
+                           configs.write.call_args_list)
+        self.juju_log.assert_called_with(
+            'Cluster leader, performing db sync'
+        )
+        self.migrate_database.assert_called_with()
+
+    @patch.object(relations, 'CONFIGS')
+    def test_db_changed_with_essex_setting_version_control(self, configs):
+        self.get_os_codename_package.return_value = "essex"
+        self.getstatusoutput.return_value = (1, "version")
+        self._shared_db_test(configs)
+        self.assertEquals([call('/etc/glance/glance-registry.conf')],
+                           configs.write.call_args_list)
+        self.check_call.assert_called_with(
+            ["glance-manage", "version_control", "0"]
+        )
+        self.juju_log.assert_called_with(
+            'Cluster leader, performing db sync'
+        )
+        self.migrate_database.assert_called_with()
 
     @patch.object(relations, 'CONFIGS')
     def test_image_service_joined(self, configs):
