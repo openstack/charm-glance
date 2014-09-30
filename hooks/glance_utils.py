988cd4e6f3777ff0abf9bcdf95725dbf0ea81957
Edward Hope-Morley <edward.hope-morley@canonical.com>
2014-09-30 14:24:38 +0100

    fixed ipv6 compatibility check

--- hooks/glance_utils.py
+++ hooks/glance_utils.py
@@ -243,9 +243,9 @@ def services():
 
 
 def setup_ipv6():
-    ubuntu_rel = float(lsb_release()['DISTRIB_RELEASE'])
-    if ubuntu_rel < 'trusty':
-        raise Exception("IPv6 is not supported for Ubuntu "
+    ubuntu_rel = lsb_release()['DISTRIB_CODENAME'].lower()
+    if ubuntu_rel < "trusty":
+        raise Exception("IPv6 is not supported in the charms for Ubuntu "
                         "versions less than Trusty 14.04")
 
     # NOTE(xianghui): Need to install haproxy(1.5.3) from trusty-backports
