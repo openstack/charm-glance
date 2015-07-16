ade8f16739b7175bee50846f37efe43548def47f
David Della Vecchia <ddv@canonical.com>
2015-07-16 18:45:41 +0000

    Adding action_managed_upgrade config option and checking for it during action execution and config-changed hook.

--- actions/openstack_upgrade.py
+++ actions/openstack_upgrade.py
@@ -3,6 +3,9 @@ import sys
 
 sys.path.append('hooks/')
 
+from charmhelpers.core.hookenv import config
+from glance_relations import config_changed
+
 from charmhelpers.contrib.openstack.utils import (
     openstack_upgrade_available,
     juju_log
@@ -13,16 +16,16 @@ from glance_utils import (
     register_configs
 )
 
-from glance_relations import (
-    config_changed
-)
 
 CONFIGS = register_configs()
 
 def openstack_upgrade():
-    juju_log('Upgrading OpenStack release')
-    do_openstack_upgrade(CONFIGS)
-    config_changed()
+    if config('action_managed_upgrade'):
+      juju_log('Upgrading OpenStack release')
+      do_openstack_upgrade(CONFIGS)
+      config_changed()
+    else:
+      juju_log('action_managed_upgrade set to false, OpenStack upgrade aborted.')
 
 if __name__ == '__main__':
     openstack_upgrade()
