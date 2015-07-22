94126a5d003209ef84c0ca816ead45e1d4cd5908
David Della Vecchia <ddv@canonical.com>
2015-07-22 18:38:42 +0000

    Fixed issues with config option action_managed_upgrade. Added conditional to upgrade action to check for git install and display error if so. Added passing unit tests for upgrade action.

--- actions/openstack_upgrade.py
+++ actions/openstack_upgrade.py
@@ -1,14 +1,21 @@
 #!/usr/bin/python
 import sys
+import traceback
 
 sys.path.append('hooks/')
 
-from charmhelpers.core.hookenv import config
+from charmhelpers.core.hookenv import (
+    action_set,
+    action_fail,
+    config
+)
+
 from glance_relations import config_changed
 
 from charmhelpers.contrib.openstack.utils import (
     openstack_upgrade_available,
-    juju_log
+    juju_log,
+    git_install_requested
 )
 
 from glance_utils import (
@@ -20,12 +27,28 @@ from glance_utils import (
 CONFIGS = register_configs()
 
 def openstack_upgrade():
-    if config('action_managed_upgrade'):
-      juju_log('Upgrading OpenStack release')
-      do_openstack_upgrade(CONFIGS)
-      config_changed()
+    """Upgrade packages to config-set Openstack version.
+
+    If the charm was installed from source we cannot upgrade it.
+    For backwards compatibility a config flag must be set for this
+    code to run, otherwise a full service level upgrade will fire
+    on config-changed."""
+
+    if git_install_requested():
+        action_fail('Openstack upgrade failed to run due to charm being installed from source. Please use git_reinstall action instead.')
     else:
-      juju_log('action_managed_upgrade set to false, OpenStack upgrade aborted.')
+        if config('action_managed_upgrade'):
+            juju_log('Upgrading OpenStack release')
+      
+            try:
+                do_openstack_upgrade(CONFIGS)
+            except:
+                action_set({'traceback': traceback.format_exc()})
+                action_fail('do_openstack_upgrade resulted in an unexpected error')
+
+            config_changed()
+        else:
+            action_fail('action_managed_upgrade set to false, OpenStack upgrade aborted.')
 
 if __name__ == '__main__':
     openstack_upgrade()
